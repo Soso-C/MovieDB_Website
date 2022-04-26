dc934c236137239cb10f578768b0824cfa794205
Soso-C <sonny.capisciolti@gmail.com>
2022-04-26 17:21:28 +0200

    update add useEffect movies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,30 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addMoviesToStore } from "../features/movie.slice";
 
 const Home = () => {
   const userinfo = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+  const movieInfo = useSelector((state) => state.movies.movies);
+  console.log(movieInfo);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_TMDB_KEY}&language=fr-Fr&page=1`)
+      .then((res) => {
+        dispatch(addMoviesToStore(res.data))
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   console.log(userinfo);
-  return <div className="bg-slate-800 h-screen w-full"></div>;
+  return (
+    <div className="bg-slate-800 h-screen w-full">
+      <h2 className="text-center text-3xl">Top rated</h2>
+    </div>
+  );
 };
 
 export default Home;
