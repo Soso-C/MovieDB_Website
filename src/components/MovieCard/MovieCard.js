5f1feb309f50205d6386701a8ac4c038dbc72499
Soso-C <sonny.capisciolti@gmail.com>
2022-04-26 20:01:27 +0200

    add toprated, add cardmovie and more css

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-const MovieCard = () => {
-    return (
-        <div>
-            
+const MovieCard = ({ movie }) => {
+
+    useEffect(()=>{
+
+    },[])
+
+  return (
+    <div className="flex flex-col justify-between min-w-[220px] w-[220px] h-[220px] p-4 rounded-xl bg-slate-600 mb-3">
+      <span className="bg-black rounded-md
+       w-12 text-white px-0.5 flex items-center justify-center">
+        <i class="fa-solid fa-star px-0.5 text-yellow-400"></i>
+        {movie.vote_average}
+      </span>
+      <div>
+        <h4 className="font-bold text-md overflow-hidden whitespace-nowrap text-ellipsis">{movie.title}</h4>
+        <p>{movie.release_date.slice(0,4)}</p>
+        <div className="flex justify-between items-baseline">
+          <button className="px-[10px] py-[5px] bg-gray-200 rounded-lg mt-2 font-medium">Details</button>
+          <button>
+            <i className="fa-solid fa-heart text-red-500"></i>
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,37 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import MovieCard from "../components/MovieCard/MovieCard";
 import { addMoviesToStore } from "../features/movie.slice";
 
 const Home = () => {
-  const userinfo = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
-  const movieInfo = useSelector((state) => state.movies.movies);
-  console.log(movieInfo);
+
+  const movieInfo = useSelector((state) => state.movies.movies.results);
 
   useEffect(() => {
     axios
-      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_TMDB_KEY}&language=fr-Fr&page=1`)
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_TMDB_KEY}&language=fr-Fr&page=1`
+      )
       .then((res) => {
-        dispatch(addMoviesToStore(res.data))
+        dispatch(addMoviesToStore(res.data));
         console.log(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
-  console.log(userinfo);
   return (
     <div className="bg-slate-800 h-screen w-full">
-      <h2 className="text-center text-3xl">Top rated</h2>
+      <div className="max-w-7xl mx-auto pt-8 px-4">
+        <h2 h2 className="text-xl text-white font-bold pt-6 mb-6">
+          Top rated
+          <i className="fa-solid fa-star text-yellow-400 text-xl ml-1"></i>
+        </h2>
+        <div className="flex gap-3 overflow-x-scroll w-full scrollbar">
+          {movieInfo && movieInfo.slice(0,12).map((movie) => <MovieCard movie={movie} />)}
+        </div>
+      </div>
     </div>
   );
 };
