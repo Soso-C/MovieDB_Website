3db99aa25b21718671372a69f4d7acf700848012
Soso-C <sonny.capisciolti@gmail.com>
2022-05-08 16:48:01 +0200

    delete inutile func and import

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-import { collection, doc, getDocs, onSnapshot } from "firebase/firestore";
-import { db } from "../utils/firebase.config";
 
 // Get Movies
 export async function getMovies(urlEndPoint) {
@@ -16,29 +14,6 @@ export const getMovieById = async (urlEndpoint) => {
   return moviesResponse.data;
 };
 
-// get Favorite movies from Firebase
-export async function getFavoriteMovies(uid) {
-  const querySnapshot = await getDocs(collection(db, "users"));
-  const docList = [];
-  // doc.data() is never undefined for query doc snapshots
-  querySnapshot.forEach((doc) => {
-    docList.push({
-      userId: doc.data().userid,
-      favoriteMovies: doc.data().favoriteMovies,
-    });
-  });
-  const result = docList.filter((user) => user.userId === uid);
-  return result;
-}
-
-
-// export async function addFavoriteMovie(uemail) {
-//   const movieLike = doc(db, "users", uemail)
-// }
-
-// export async function getFavoriteMovies2(uemail) {
-//   onSnapshot(doc(db, "users", uemail))
-// }
 
 // Fetch Query Movies
 export async function getQueryMovies(urlEndpoint) {
